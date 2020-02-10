524a820a73cc8a0fe6aae28db129278f9cec3cc6
JohnHG1 <honisz@yahoo.com>
2020-02-10 11:03:55 +0000

    reworked controllers

---
+++ controllers/stock_controller.rb
@@ -0,0 +1,14 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require_relative( '../models/stock.rb' )
+also_reload( '../models/*' )
+
+get '/stocks' do
+  @stocks = Stock.all()
+  erb ( :"stocks/index" )
+end
+
+get '/stocks/:id' do
+  @stock = Stock.find(params['id'].to_i)
+  erb( :"stocks/show" )
+end
--- controllers/supplier_controller.rb
+++ controllers/supplier_controller.rb
@@ -5,3 +5,26 @@ require_relative( '../models/supplier.rb' )
 require_relative( '../models/product.rb' )
 require_relative( '../models/stock.rb' )
 also_reload( '../models/*' )
+
+get '/suppliers' do
+  @suppliers = Supplier.all
+  @actions = Action.all
+  erb ( :"suppliers/index" )
+end
+
+get '/suppliers/new' do
+  @stocks = Stock.all
+  @products = Product.all
+  erb(:"suppliers/new")
+end
+
+post '/suppliers' do
+  supplier = Supplier.new(params)
+  supplier.save
+  redirect to("/suppliers")
+end
+
+post '/suppliers/:id/delete' do
+  Supplier.destroy(params[:id])
+  redirect to("/suppliers")
+end
